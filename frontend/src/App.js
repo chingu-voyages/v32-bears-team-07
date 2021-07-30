0d60b8f5227de7035a48f2a0cdb5123f06bbd437
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-07-30 18:15:33 -0400

    Add Login and Sign Up Component to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,35 @@
-import React from 'react'
-import {  Container } from 'react-bootstrap'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import React, { useState } from "react";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
 
 const App = () => {
+  const [displayLogin, setDisplayLogin] = useState(true);
+
+  const toggleDisplayLogin = () => {
+    setDisplayLogin(!displayLogin);
+  };
+
   return (
     <>
-    <Header /> 
-    <main className='py-3'>
-      <Container>
-        <h1>Welcome to popup shop</h1>
-      </Container>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to popup shop</h1>
+        </Container>
 
-    </main>
-      
+        {displayLogin ? (
+          <Login toggleDisplayLogin={toggleDisplayLogin} />
+        ) : (
+          <SignUp />
+        )}
+      </main>
+
       <Footer />
     </>
   );
-}
+};
 
 export default App;
