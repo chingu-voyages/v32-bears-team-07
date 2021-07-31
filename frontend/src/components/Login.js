04e24d8d17c9527465ed45a64713d034949b155b
Kyle Eriss <kyle.eriss@gmail.com>
2021-07-31 14:30:03 -0400

    login and signup connected to backend server

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,8 +1,30 @@
 import React from "react";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import AuthApiService from '../Api-Service';
 import "./login.css";
 
 const Login = (props) => {
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    // this.setState({ error: null })
+    const { username, password } = event.target
+
+    AuthApiService.postLogin({
+      username: username.value,
+      password: password.value,
+    })
+      .then(res => {
+        username.value = ''
+        password.value = ''
+        // TokenService.saveAuthToken(res.authToken)
+        // this.props.onLoginSuccess()
+      })
+      // .catch(res => {
+      //   this.setState({ error: res.error })
+      // })
+  }
+
   return (
     <div>
       <Container className="loginContainer">
@@ -10,13 +32,13 @@ const Login = (props) => {
           <Col className="text-center">
             <Form>
               <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Label>Username</Form.Label>
+                <Form.Control name="username" type="username" placeholder="Enter username" />
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
+                <Form.Control name="password" type="password" placeholder="Password" />
               </Form.Group>
               <Button variant="primary" type="submit">
                 Login
@@ -25,7 +47,7 @@ const Login = (props) => {
                 <Button
                   variant="light"
                   size="sm"
-                  type="submit"
+                  type="button"
                   onClick={props.toggleDisplayLogin}
                 >
                   Create New Account
--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -1,33 +1,57 @@
-import React from "react";
+import { React, useState } from "react";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import AuthApiService from '../Api-Service';
 import "./signup.css";
 
 const SignUp = () => {
+
+  // const {error, setError} = useState(null)
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    const { username, email, password } = event.target
+
+    // setError({ error: null })
+    AuthApiService.postUser({
+      username: username.value,
+      email: email.value,
+      password: password.value,
+    })
+      .then(user => {
+        username.value = ''
+        password.value = ''
+        // this.props.onRegistrationSuccess()
+      })
+    // .catch(res => {
+    //   setError({ error: res.error })
+    // })
+  }
+
   return (
     <div>
       <Container className="signUpContainer">
         <Row md={1.5} lg={2} className="justify-content-center">
           <Col className="text-center">
-            <Form>
+            <Form onSubmit={handleSubmit} >
               <Form.Group className="mb-3" controlId="formBasicName">
                 <Form.Label>Name</Form.Label>
-                <Form.Control type="name" placeholder="Enter name" />
+                <Form.Control name="username" type="username" placeholder="Enter name" />
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Control name="email" type="email" placeholder="Enter email" />
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
+                <Form.Control name="password" type="password" placeholder="Password" />
               </Form.Group>
 
-              <Form.Group className="mb-3" controlId="formBasicPassword">
+              {/* <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label>Confirm Password</Form.Label>
                 <Form.Control type="password" placeholder="Password" />
-              </Form.Group>
+              </Form.Group> */}
               <Button variant="primary" type="submit">
                 Sign Up
               </Button>
