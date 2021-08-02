bed70d0f8bf7d903358ae198c46ade0e5195d81c
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-08-02 17:35:13 -0400

    added side panel for GrapesJS

--- frontend/src/components/GrapesJS.js
+++ frontend/src/components/GrapesJS.js
@@ -50,6 +50,129 @@ const GrapesJS = () => {
           },
         ],
       },
+      layerManager: {
+        appendTo: ".layers-container",
+      },
+      // We define a default panel as a sidebar to contain layers
+      panels: {
+        defaults: [
+          {
+            id: "layers",
+            el: ".panel__right",
+            // Make the panel resizable
+            resizable: {
+              maxDim: 350,
+              minDim: 200,
+              tc: 0, // Top handler
+              cl: 1, // Left handler
+              cr: 0, // Right handler
+              bc: 0, // Bottom handler
+              // Being a flex child we need to change `flex-basis` property
+              // instead of the `width` (default)
+              keyWidth: "flex-basis",
+            },
+          },
+        ],
+      },
+      panels: {
+        defaults: [
+          // ...
+          {
+            id: "panel-switcher",
+            el: ".panel__switcher",
+            buttons: [
+              {
+                id: "show-layers",
+                active: true,
+                label: "Layers",
+                command: "show-layers",
+                // Once activated disable the possibility to turn it off
+                togglable: false,
+              },
+              {
+                id: "show-style",
+                active: true,
+                label: "Styles",
+                command: "show-styles",
+                togglable: false,
+              },
+            ],
+          },
+        ],
+      },
+      // The Selector Manager allows to assign classes and
+      // different states (eg. :hover) on components.
+      // Generally, it's used in conjunction with Style Manager
+      // but it's not mandatory
+      selectorManager: {
+        appendTo: ".styles-container",
+      },
+      styleManager: {
+        appendTo: ".styles-container",
+        sectors: [
+          {
+            name: "Dimension",
+            open: false,
+            // Use built-in properties
+            buildProps: ["width", "min-height", "padding"],
+            // Use `properties` to define/override single property
+            properties: [
+              {
+                // Type of the input,
+                // options: integer | radio | select | color | slider | file | composite | stack
+                type: "integer",
+                name: "The width", // Label for the property
+                property: "width", // CSS property (if buildProps contains it will be extended)
+                units: ["px", "%"], // Units, available only for 'integer' types
+                defaults: "auto", // Default value
+                min: 0, // Min value, available only for 'integer' types
+              },
+            ],
+          },
+          {
+            name: "Extra",
+            open: false,
+            buildProps: ["background-color", "box-shadow", "custom-prop"],
+            properties: [
+              {
+                id: "custom-prop",
+                name: "Custom Label",
+                property: "font-size",
+                type: "select",
+                defaults: "32px",
+                // List of options, available only for 'select' and 'radio'  types
+                options: [
+                  { value: "12px", name: "Tiny" },
+                  { value: "18px", name: "Medium" },
+                  { value: "32px", name: "Big" },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+      panels: {
+        defaults: [
+          // ...
+          {
+            id: "panel-switcher",
+            el: ".panel__switcher",
+            buttons: [
+              // ...
+              {
+                id: "show-traits",
+                active: true,
+                label: "Traits",
+                command: "show-traits",
+                togglable: false,
+              },
+            ],
+          },
+        ],
+      },
+      traitManager: {
+        appendTo: ".traits-container",
+      },
     });
 
     editor.BlockManager.add("my-block-id", {
@@ -110,6 +233,44 @@ const GrapesJS = () => {
           },
         },
       ],
+      deviceManager: {
+        devices: [
+          {
+            name: "Desktop",
+            width: "", // default size
+          },
+          {
+            name: "Mobile",
+            width: "320px", // this value will be used on canvas width
+            widthMedia: "480px", // this value will be used in CSS @media
+          },
+        ],
+      },
+      // ...
+      panels: {
+        defaults: [
+          // ...
+          {
+            id: "panel-devices",
+            el: ".panel__devices",
+            buttons: [
+              {
+                id: "device-desktop",
+                label: "D",
+                command: "set-device-desktop",
+                active: true,
+                togglable: false,
+              },
+              {
+                id: "device-mobile",
+                label: "M",
+                command: "set-device-mobile",
+                togglable: false,
+              },
+            ],
+          },
+        ],
+      },
     });
     editor.on("run:export-template:before", (opts) => {
       console.log("Before the command run");
@@ -121,15 +282,83 @@ const GrapesJS = () => {
       console.log("After the command run")
     );
     editor.on("abort:export-template", () => console.log("Command aborted"));
+
+    // Define commands
+    editor.Commands.add("show-layers", {
+      getRowEl(editor) {
+        return editor.getContainer().closest(".editor-row");
+      },
+      getLayersEl(row) {
+        return row.querySelector(".layers-container");
+      },
+
+      run(editor, sender) {
+        const lmEl = this.getLayersEl(this.getRowEl(editor));
+        lmEl.style.display = "";
+      },
+      stop(editor, sender) {
+        const lmEl = this.getLayersEl(this.getRowEl(editor));
+        lmEl.style.display = "none";
+      },
+    });
+    editor.Commands.add("show-styles", {
+      getRowEl(editor) {
+        return editor.getContainer().closest(".editor-row");
+      },
+      getStyleEl(row) {
+        return row.querySelector(".styles-container");
+      },
+
+      run(editor, sender) {
+        const smEl = this.getStyleEl(this.getRowEl(editor));
+        smEl.style.display = "";
+      },
+      stop(editor, sender) {
+        const smEl = this.getStyleEl(this.getRowEl(editor));
+        smEl.style.display = "none";
+      },
+    });
+    // Define command
+    // ...
+    editor.Commands.add("show-traits", {
+      getTraitsEl(editor) {
+        const row = editor.getContainer().closest(".editor-row");
+        return row.querySelector(".traits-container");
+      },
+      run(editor, sender) {
+        this.getTraitsEl(editor).style.display = "";
+      },
+      stop(editor, sender) {
+        this.getTraitsEl(editor).style.display = "none";
+      },
+    });
+    // Commands
+    editor.Commands.add("set-device-desktop", {
+      run: (editor) => editor.setDevice("Desktop"),
+    });
+    editor.Commands.add("set-device-mobile", {
+      run: (editor) => editor.setDevice("Mobile"),
+    });
   });
 
   return (
     <Container>
       <div className="panel__top">
         <div className="panel__basic-actions"></div>
+        <div class="panel__devices"></div>
+        <div class="panel__switcher"></div>
       </div>
-      <div id="gjs">
-        <h1>Hello World Component!</h1>
+      <div class="editor-row">
+        <div class="editor-canvas">
+          <div id="gjs">
+            <h1>Hello World Component!</h1>
+          </div>
+        </div>
+        <div class="panel__right">
+          <div class="layers-container"></div>
+          <div class="styles-container"></div>
+          <div class="traits-container"></div>
+        </div>
       </div>
       <div id="blocks"></div>
     </Container>
