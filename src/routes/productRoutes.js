bbb3c5b44f088d5c77b27c5a6f2edfe4a0818a25
Ryan <leeyuenhigh@gmail.com>
2021-08-02 21:18:15 -0400

    Added update product route

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,47 +1,63 @@
 const router = require("express").Router();
-const Product = require("../models/Product")
+const Product = require("../models/Product");
 
 // Create product
-router.post("/",async (req,res) => {
-    try{
-        const newProduct = new Product({
-            name: req.body.name,
-            description: req.body.description,
-            price: req.body.price,
-            stock: req.body.stock,
-            digitalProduct: req.body.digitalProduct
-        })
+router.post("/", async (req, res) => {
+  try {
+    const newProduct = new Product({
+      name: req.body.name,
+      description: req.body.description,
+      price: req.body.price,
+      stock: req.body.stock,
+      digitalProduct: req.body.digitalProduct,
+    });
 
-        const product = await newProduct.save();
-        res.status(200).json(product);
-    }catch(err){
-        res.status(500).json(err)
-    }
-})
+    const product = await newProduct.save();
+    res.status(200).json(product);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // Get product
-router.get("/:productId", async (req,res) => {
-    try{
-        const product = await Product.findById(req.params.productId);
-        res.status(200).json(product);
-    }catch(err){
-        res.status(404).json("Product not found")
-    }
-})
+router.get("/:productId", async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.productId);
+    res.status(200).json(product);
+  } catch (err) {
+    res.status(404).json("Product not found");
+  }
+});
+
+// Update Product
+router.patch("/:productId", async (req, res) => {
+  try {
+    const productInfoToUpdate = await Product.findByIdAndUpdate(
+      req.params.productId,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(productInfoToUpdate);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // Delete product
-router.delete("/:productId", async (req,res) => {
-    try{
-        await Product.findById(req.params.productId);
-        try{
-            await Product.findByIdAndDelete(req.params.productId);
-            res.status(200).json("Product deleted")
-        }catch(err){
-            res.status(404).json(err)
-        }
-    }catch(err){
-        res.status(404).json("Product not found")
+router.delete("/:productId", async (req, res) => {
+  try {
+    await Product.findById(req.params.productId);
+    try {
+      await Product.findByIdAndDelete(req.params.productId);
+      res.status(200).json("Product deleted");
+    } catch (err) {
+      res.status(404).json(err);
     }
-})
+  } catch (err) {
+    res.status(404).json("Product not found");
+  }
+});
 
 module.exports = router;
