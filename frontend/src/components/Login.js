5434feeb3cf2fbff90ebd2cf168fc0dcda2355b5
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-08-11 10:48:16 -0400

    connected login buttons using react router dom

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,28 +1,28 @@
 import React from "react";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
-import AuthApiService from '../Api-Service';
+import AuthApiService from "../Api-Service";
 import "./login.css";
+import { Switch, Route, Link } from "react-router-dom";
+import SignUp from "./SignUp";
 
-const Login = (props) => {
-
+const Login = () => {
   function handleSubmit(event) {
-    event.preventDefault()
+    event.preventDefault();
 
-    const { username, password } = event.target
+    const { username, password } = event.target;
 
     AuthApiService.postLogin({
       username: username.value,
       password: password.value,
-    })
-      .then(res => {
-        username.value = ''
-        password.value = ''
-        // TokenService.saveAuthToken(res.authToken)
-        // this.props.onLoginSuccess()
-      })
-      // .catch(res => {
-      //   this.setState({ error: res.error })
-      // })
+    }).then((res) => {
+      username.value = "";
+      password.value = "";
+      // TokenService.saveAuthToken(res.authToken)
+      // this.props.onLoginSuccess()
+    });
+    // .catch(res => {
+    //   this.setState({ error: res.error })
+    // })
   }
 
   return (
@@ -33,30 +33,38 @@ const Login = (props) => {
             <Form onSubmit={handleSubmit}>
               <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Username</Form.Label>
-                <Form.Control name="username" type="username" placeholder="Enter username" />
+                <Form.Control
+                  name="username"
+                  type="username"
+                  placeholder="Enter username"
+                />
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label>Password</Form.Label>
-                <Form.Control name="password" type="password" placeholder="Password" />
+                <Form.Control
+                  name="password"
+                  type="password"
+                  placeholder="Password"
+                />
               </Form.Group>
               <Button variant="primary" type="submit">
                 Login
               </Button>
               <Col className="mt-4">
-                <Button
-                  variant="light"
-                  size="sm"
-                  type="button"
-                  onClick={props.toggleDisplayLogin}
-                >
-                  Create New Account
-                </Button>
+                <Link to="/signup">
+                  <Button variant="light" size="sm" type="button">
+                    Create New Account
+                  </Button>
+                </Link>
               </Col>
             </Form>
           </Col>
         </Row>
       </Container>
+      <Switch>
+        <Route path="/signup" component={SignUp} />
+      </Switch>
     </div>
   );
 };
