fa68d774e4e4d8eed1eabe27cd6bb7a10eb7eeaa
Kyle Eriss <kyle.eriss@gmail.com>
2021-08-11 17:45:59 -0400

    config edits

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,8 @@
 export default {
-    API_ENDPOINT: 'http://localhost:5000/api/auth',
+    // API_ENDPOINT: 'http://localhost:5000/api/auth',
+    API_ENDPOINT: 'https://lit-journey-88324.herokuapp.com',
     TOKEN_KEY: 'popupshop-client-auth-token',    
   };
+
+
+  
