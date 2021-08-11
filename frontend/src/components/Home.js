147557d42c9beff789b3cae48422790c17e9cc66
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-08-11 10:47:18 -0400

    Home.js and home.css for landing page

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Container, Button } from "react-bootstrap";
+import "./home.css";
+
+const Home = () => {
+  return (
+    <div>
+      <Container className="homeContainer">
+        <h1>Welcome to popup shop</h1>
+      </Container>
+    </div>
+  );
+};
+
+export default Home;
