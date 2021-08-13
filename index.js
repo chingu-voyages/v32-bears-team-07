d2d9d16a8410dac915310241b3de492d38485f07
Kyle Eriss <kyle.eriss@gmail.com>
2021-08-13 12:30:10 -0400

    cors troubleshooting

--- index.js
+++ index.js
@@ -14,11 +14,25 @@ app.use(express.json());
 app.use(helmet())
 app.use(
   cors({
-    origin: "*",
-    methods: ["GET", "POST", "DELETE"],
-    allowedHeaders: ["content-type"],
+    origin: '*',
   })
 );
+
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  }
+  else {
+    next();
+  }
+};
+
+app.use(allowCrossDomain);
 
 mongoose
   .connect(process.env.MONGODB_URI, {
