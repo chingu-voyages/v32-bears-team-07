bf07d3e58d1316fcf8f88d4ebab1a9e576bd9277
Kyle Eriss <kyle.eriss@gmail.com>
2021-08-15 12:00:59 -0400

    cors issues

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-const cors = require("cors");
+// const cors = require("cors");
 const helmet = require('helmet');
 const authenticationRoute = require("./src/routes/authentication");
 const userFunctionsRoute = require("./src/routes/userFunctions");
@@ -14,11 +14,11 @@ app.use(express.json());
 const PORT = process.env.PORT || 5000
 
 app.use(helmet())
-app.use(
-  cors({
-    origin: '*',
-  })
-);
+// app.use(
+//   cors({
+//     origin: '*',
+//   })
+// );
 
 mongoose
   .connect(process.env.MONGODB_URI, {
