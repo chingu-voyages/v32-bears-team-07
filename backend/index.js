fc800cb5b37595452f23e46ed4dfd9833eee5064
Kyle Eriss <kyle.eriss@gmail.com>
2021-08-15 12:25:00 -0400

    index edits

--- backend/index.js
+++ backend/index.js
@@ -42,7 +42,7 @@ app.use("/api/productRoutes", productRoutes);
 const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')))
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
 
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
