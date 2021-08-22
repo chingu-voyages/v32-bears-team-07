cba3de8c660e722cffafc2ddd5b1a02069fff848
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-08-22 18:20:23 -0400

    product card for each product - image, title, price, add to cart button

---
+++ frontend/src/components/Product.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Container, Card, Button } from "react-bootstrap";
+import "./Product.css";
+
+export default function Product(props) {
+  return (
+    <div>
+      <Container>
+        <Card className=".card">
+          <Card.Img className="image" variant="top" src={props.img} />
+          <Card.Title>{props.title}</Card.Title>
+          <Card.Text>${props.price}</Card.Text>
+          <Button variant="primary">Add to Cart</Button>
+        </Card>
+      </Container>
+    </div>
+  );
+}
