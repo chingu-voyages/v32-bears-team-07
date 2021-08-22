f9e2d204edbf20426498b9e4ee2e3f045ecf0189
Ryan <leeyuenhigh@gmail.com>
2021-08-22 10:09:56 -0400

    Added endpoints for multiparty stripe

--- backend/index.js
+++ backend/index.js
@@ -18,6 +18,13 @@ const PORT = process.env.PORT || 5000;
 
 app.use(helmet());
 app.use(cors());
+app.use(
+  session({
+    secret: process.env.Session_Secret,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 //process.env.MONGODB_URI
 
--- backend/src/routes/stripeRoutes.js
+++ backend/src/routes/stripeRoutes.js
@@ -4,6 +4,7 @@ dotenv.config();
 const stripe = require("stripe")(process.env.Stripe_Secret_Key);
 const { v4: uuidv4 } = require("uuid");
 
+// Route for single party stripe integration
 router.post("/", (req, res) => {
   const { product, token } = req.body;
   // Created to ensure that the customer is only charged once
@@ -41,6 +42,52 @@ router.post("/", (req, res) => {
       .then((result) => res.status(200).json(result))
       .catch((err) => console.log(err))
   );
+});
+
+// Helper function for endpoints to follow
+function generateAccountLink(accountID, origin) {
+  return stripe.accountLinks
+    .create({
+      type: "account_onboarding",
+      account: accountID,
+      refresh_url: `${origin}/onboard-user/refresh`,
+      return_url: `${origin}/success.html`,
+    })
+    .then((link) => link.url);
+}
+
+// Multiparty stripe integration with standard connect account
+router.post("/onboard-user", async (req, res) => {
+  try {
+    const account = await stripe.accounts.create({ type: "standard" });
+    req.session.accountID = account.id;
+
+    const origin = `${req.headers.origin}`;
+    const accountLinkURL = await generateAccountLink(account.id, origin);
+    res.send({ url: accountLinkURL });
+  } catch (err) {
+    res.status(500).send({
+      error: err.message,
+    });
+  }
+});
+
+router.get("/onboard-user/refresh", async (req, res) => {
+  if (!req.session.accountID) {
+    res.redirect("/");
+    return;
+  }
+  try {
+    const { accountID } = req.session;
+    const origin = `${req.secure ? "https://" : "https://"}${req.headers.host}`;
+
+    const accountLinkURL = await generateAccountLink(accountID, origin);
+    res.redirect(accountLinkURL);
+  } catch (err) {
+    res.status(500).send({
+      error: err.message,
+    });
+  }
 });
 
 module.exports = router;
