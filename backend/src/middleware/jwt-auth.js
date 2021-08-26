94b51448395c1f3700db1da3d692cf368d62d64c
Ryan <leeyuenhigh@gmail.com>
2021-08-26 02:02:20 -0400

    Assigned payload value to payload const after verification

--- backend/src/middleware/jwt-auth.js
+++ backend/src/middleware/jwt-auth.js
@@ -1,32 +1,33 @@
-const AuthService = require('../routes/auth-service');
-const User = require("../models/User");
+const AuthService = require("../routes/auth-service");
 
 function requireAuth(req, res, next) {
-    const authToken = req.get('Authorization') || ''
+  const authToken = req.get("Authorization") || "";
+  let bearerToken;
 
-    let bearerToken
-    if (!authToken.toLowerCase().startsWith('bearer ')) {
-        return res.status(401).json({ error: 'Missing bearer token' })
-    } else {
-        bearerToken = authToken.slice(7, authToken.length)
-    }
+  if (!authToken.toLowerCase().startsWith("bearer ")) {
+    return res.status(401).json({ error: "Missing bearer token" });
+  } else {
+    bearerToken = authToken.slice(7, authToken.length);
+  }
+  try {
+    // Assigned the payload value to payload after verification
+    const payload = AuthService.verifyJwt(bearerToken);
+    // AuthService.getUserWithUserName(payload.sub).then((user) => {
+    //   if (!user) return res.status(401).json({ error: "Unauthorized request" });
 
-    try {
-        AuthService.verifyJwt(bearerToken)
+    //   req.user = user;
+    //   next();
+    // });
 
-        AuthService.getUserWithUserName(
-            payload.sub,
-        )
-            .then(user => {
-                if (!user)
-                    return res.status(401).json({ error: 'Unauthorized request' })
-
-                req.user = user
-                next()
-            })
-    } catch (error) {
-        res.status(401).json({ error: 'Unauthorized request' })
-    }
+    // payload was previously undefined
+    AuthService.getUserById(payload.sub).then((user) => {
+      if (!user) return res.status(401).json({ error: "Unauthorized request" });
+      req.user = user;
+      next();
+    });
+  } catch (error) {
+    res.status(401).json({ error: "Unauthorized request" });
+  }
 }
 
-module.exports = requireAuth
+module.exports = requireAuth;
