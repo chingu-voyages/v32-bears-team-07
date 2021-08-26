c8e0559ce63c4cf5d2883fde3267eba84210f930
Ryan <leeyuenhigh@gmail.com>
2021-08-26 00:24:14 -0400

    Testing User._id field for payload sub

--- backend/src/routes/auth-service.js
+++ backend/src/routes/auth-service.js
@@ -1,50 +1,50 @@
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
-const REGEX_UPPER_LOWER_NUMBER_SPECIAL = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&])[\S]+/
+const REGEX_UPPER_LOWER_NUMBER_SPECIAL =
+  /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&])[\S]+/;
 const emailValidator = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 const AuthService = {
+  validatePassword(username, email, password) {
+    if (password.length < 8) {
+      return "password must be at least 8 characters";
+    }
+    if (password.length > 72) {
+      return "password must be less than 72 characters";
+    }
+    if (password.startsWith(" ") || password.endsWith(" ")) {
+      return "password must not start or end with empty spaces";
+    }
+    if (!REGEX_UPPER_LOWER_NUMBER_SPECIAL.test(password)) {
+      return "password must contain one upper case, lower case, symbol, and number";
+    }
+    if (!emailValidator.test(email)) {
+      return "please use a valid email address";
+    }
+    if (password === username || password === email) {
+      return "username or email may not be used as password";
+    }
+    return null;
+  },
+  hashPassword(password) {
+    return bcrypt.hash(password, 12);
+  },
+  createJwt(subject, payload) {
+    return jwt.sign(payload, process.env.JWT_SECRET, {
+      subject,
+      algorithm: "HS256",
+    });
+  },
+  getUserWithUserName(id) {
+    return User.findById(id);
+  },
+  verifyJwt(token) {
+    return jwt.verify(token, process.env.JWT_SECRET, {
+      algorithms: ["HS256"],
+    });
+  },
+};
 
-    validatePassword(username, email, password) {
-        if (password.length < 8) {
-            return 'password must be at least 8 characters'
-        }
-        if (password.length > 72) {
-            return 'password must be less than 72 characters'
-        }
-        if (password.startsWith(' ') || password.endsWith(' ')) {
-            return 'password must not start or end with empty spaces'
-        }
-        if (!REGEX_UPPER_LOWER_NUMBER_SPECIAL.test(password)) {
-            return 'password must contain one upper case, lower case, symbol, and number'
-        }
-        if (!emailValidator.test(email)) {
-            return 'please use a valid email address'
-        }
-        if (password === username || password === email) {
-            return 'username or email may not be used as password'
-        }
-        return null
-    },
-    hashPassword(password) {
-        return bcrypt.hash(password, 12)
-    },
-    createJwt(subject, payload) {
-        return jwt.sign(payload, process.env.JWT_SECRET, {
-            subject,
-            algorithm: 'HS256',
-        })
-    },
-    getUserWithUserName(username) {
-        return User.findOne({ username })
-    },
-    verifyJwt(token) {
-        return jwt.verify(token, process.env.JWT_SECRET, {
-            algorithms: ['HS256'],
-        })
-    },
-}
-
-module.exports = AuthService
+module.exports = AuthService;
--- backend/src/routes/authentication.js
+++ backend/src/routes/authentication.js
@@ -10,38 +10,39 @@ router.post("/register", async (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
 
-    for (const field of ['username', 'email', 'password'])
-      if (!req.body[field])
-        return res.status(400).json(
-          `missing ${field}`
-        )
+    for (const field of ["username", "email", "password"])
+      if (!req.body[field]) return res.status(400).json(`missing ${field}`);
 
-    const hashedPass = await AuthService.hashPassword(password)
+    const hashedPass = await AuthService.hashPassword(password);
     const newUser = new User({
       username: req.body.username,
       email: req.body.email,
-      password: hashedPass
+      password: hashedPass,
     });
 
     // checks for duplicate username
     const usernameCheck = await User.findOne({ username: req.body.username });
-    if (usernameCheck) return res.status(400).json("username already registered");
+    if (usernameCheck)
+      return res.status(400).json("username already registered");
 
     // checks for duplicate email
     const emailCheck = await User.findOne({ email: req.body.email });
     if (emailCheck) return res.status(400).json("email already registered");
 
-    const passwordError = AuthService.validatePassword(username, email, password)
-    if (passwordError) return res.status(400).json(passwordError)
+    const passwordError = AuthService.validatePassword(
+      username,
+      email,
+      password
+    );
+    if (passwordError) return res.status(400).json(passwordError);
 
     const user = await newUser.save();
-    
-    const sub = username
-    const payload = { user_id: User._id }
+
+    const sub = username;
+    const payload = { user_id: User._id };
     res.send({
       authToken: AuthService.createJwt(sub, payload),
-    })
-
+    });
   } catch (err) {
     res.status(500).json(err);
   }
@@ -50,15 +51,11 @@ router.post("/register", async (req, res) => {
 //LOGIN
 router.post("/login", async (req, res) => {
   try {
-
     const username = req.body.username;
     // const password = req.body.password;
 
-    for (const field of ['username', 'password'])
-      if (!req.body[field])
-        return res.status(400).json(
-          `missing ${field}`
-        )
+    for (const field of ["username", "password"])
+      if (!req.body[field]) return res.status(400).json(`missing ${field}`);
 
     const user = await User.findOne({ username: req.body.username });
     if (!user) return res.status(401).json("Wrong credentials!");
@@ -69,12 +66,11 @@ router.post("/login", async (req, res) => {
     // const { password, paymentInfo, ...info } = user._doc;
     // res.status(200).json(info);
 
-    const sub = username
-    const payload = { user_id: User._id }
+    const sub = User._id;
+    const payload = { user_id: User._id };
     res.send({
       authToken: AuthService.createJwt(sub, payload),
-    })
-
+    });
   } catch (err) {
     res.status(401).json(err);
   }
