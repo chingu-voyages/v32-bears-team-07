71dd39c4c9e1f925b667558e62a949e8cc817f0c
Ryan <leeyuenhigh@gmail.com>
2021-08-25 20:59:16 -0400

    Added post route for order

---
+++ backend/src/routes/orderRoutes.js
@@ -0,0 +1,29 @@
+const router = require("express").Router();
+const Order = require("../models/Order");
+const User = reqruie("../models/User");
+
+// Create order
+router.post("/", async (req, res) => {
+  const { userId, orderItems, total } = req.body;
+  let newOrder = { userId, orderItems, total };
+  for (const [key, value] of Object.entries(newOrder)) {
+    if (value == null) {
+      return res.status(400).json({
+        error: { message: `Missing '${key}' in request body` },
+      });
+    }
+  }
+  try {
+    await User.findById(userId);
+    try {
+      newOrder = new Order(newOrder);
+
+      const order = await newOrder.save();
+      res.status(200).json(order);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } catch (err) {
+    res.status(404).json("User not found");
+  }
+});
