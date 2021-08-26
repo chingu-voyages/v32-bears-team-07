ae8970f865eeefec49653055bba12b858aa223d1
Ryan <leeyuenhigh@gmail.com>
2021-08-25 23:40:17 -0400

    Updated package-lock.json files

--- frontend/src/Api-Service.js
+++ frontend/src/Api-Service.js
@@ -1,65 +1,53 @@
-import config from './config';
+import config from "./config";
+import TokenService from "./token-service";
 
 const AuthApiService = {
-    postLogin({ username, password }) {
-        return fetch(`${config.API_ENDPOINT}/auth/login`, {
-            method: 'POST',
-            headers: {
-                'content-type': 'application/json',
-            },
-            body: JSON.stringify({ username, password }),
-        })
-            .then(res =>
-                (!res.ok)
-                    ? res.json().then(e => Promise.reject(e))
-                    : res.json()
-            )
-    },
+  postLogin({ username, password }) {
+    return fetch(`${config.API_ENDPOINT}/auth/login`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    }).then((res) =>
+      !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
+    );
+  },
 
+  postUser(user) {
+    return fetch(`${config.API_ENDPOINT}/auth/register`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    }).then((res) =>
+      !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
+    );
+  },
 
-    postUser(user) {
-        return fetch(`${config.API_ENDPOINT}/auth/register`, {
-            method: 'POST',
-            headers: {
-                'content-type': 'application/json',
-            },
-            body: JSON.stringify(user),
-        })
-            .then(res =>
-                (!res.ok)
-                    ? res.json().then(e => Promise.reject(e))
-                    : res.json()
-            )
-    },
+  getProducts() {
+    return fetch(`${config.API_ENDPOINT}/productRoutes`, {
+      method: "GET",
+      headers: {
+        "content-type": "application/json",
+      },
+    }).then((res) =>
+      !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
+    );
+  },
 
-    getProducts() {
-        return fetch(`${config.API_ENDPOINT}/productRoutes`, {
-            method: 'GET',
-            headers: {
-                'content-type': 'application/json',
-            }
-        })
-            .then(res =>
-                (!res.ok)
-                    ? res.json().then(e => Promise.reject(e))
-                    : res.json()
-            )
-    },
+  getCartProducts() {
+    return fetch(`${config.API_ENDPOINT}/cartRoutes`, {
+      method: "GET",
+      headers: {
+        "content-type": "application/json",
+        authorization: `bearer ${TokenService.getAuthToken()}`,
+      },
+    }).then((res) =>
+      !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
+    );
+  },
+};
 
-    getCartProducts() {
-        return fetch(`${config.API_ENDPOINT}/cartRoutes`, {
-            method: 'GET',
-            headers: {
-                'content-type': 'application/json',
-                'authorization': `bearer ${TokenService.getAuthToken()}`,
-            }
-        })
-            .then(res =>
-                (!res.ok)
-                    ? res.json().then(e => Promise.reject(e))
-                    : res.json()
-            )
-    }
-}
-
-export default AuthApiService
+export default AuthApiService;
